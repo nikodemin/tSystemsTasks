b8e76a4cd2999a79f7615e6a3f29d29d4fdcd0e5
Nikita Dyomin <niko.demin@gmail,com>
2019-06-25 12:07:10 +0300

    rewrited calculator

--- src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
+++ src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
@@ -1,6 +1,8 @@
 package com.tsystems.javaschool.tasks.calculator;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Calculator
 {
@@ -19,59 +21,92 @@ public class Calculator
         if(statement == null)
             return null;
 
-        String[] numsStr = statement.split("[\\(\\)\\-\\+\\*\\/]");
-        List<String> operations = new ArrayList<>(Arrays.asList(statement.split("[0-9]+(\\.[0-9])*")));
-        Deque<Double> nums = new ArrayDeque<>();
-        try
+        statement = statement.replace(" ","");
+
+        Pattern pattern = Pattern.compile("\\((.+)\\)");
+        Matcher matcher = pattern.matcher(statement);
+        while (matcher.find())
         {
-            for (String num : numsStr)
-                nums.add(Double.parseDouble(num));
+            String subQuery = matcher.group(1);
+            System.out.println("OLD="+statement);
+            statement = statement.replace("("+subQuery+")",evaluate(subQuery));
+            System.out.println("NEW="+statement);
         }
-        catch (Exception e)
+
+        System.out.println("STATEMENT0="+statement);
+        List<String> nums = new ArrayList<>();
+        pattern = Pattern.compile("-?[0-9]+");
+        matcher = pattern.matcher(statement);
+        while (matcher.find())
         {
-            e.printStackTrace();
+            nums.add(matcher.group(0));
+            System.out.println("MATCHER="+matcher.group(0));
+        }
+        if(nums.size()>2)
+        {
+            //division
+            statement = evalSubQuery(statement, "-?[0-9]+\\/[0-9]+");
+            //multiplication
+            statement = evalSubQuery(statement, "-?[0-9]+\\*[0-9]+");
+            //substract
+            statement = evalSubQuery(statement, "-?[0-9]+\\-[0-9]+");
+            //addition
+            statement = evalSubQuery(statement, "-?[0-9]+\\+[0-9]+");
+        }
+
+        //here statement will be like 4+3 or 1/3
+        System.out.println("STATEMENT="+statement);
+        System.out.println(statement.split("[\\/\\*\\+\\-]").length);
+        if (statement.split("[\\/\\*\\+\\-]").length==1)
+            return statement;
+        Double leftOp = Double.parseDouble(nums.get(0));
+        Double rigthOp = Double.parseDouble(nums.get(1));
+        String operator;
+        Double result = leftOp;
+
+        pattern = Pattern.compile("[0-9]+([\\*\\/\\+\\-])[0-9]+");
+        matcher = pattern.matcher(statement);
+        if (matcher.find())
+            operator = matcher.group(1);
+        else
             return null;
-        }
-
-        for (int i = 0; i < operations.size(); i++)
-            if(operations.get(i).matches("[\t\n\r]"))
-                operations.remove(i);
-        for (int i = 0; i < operations.size(); i++)
+        System.out.println("OPERATOR="+operator);
+        System.out.println("left="+leftOp);
+        System.out.println("rigth="+rigthOp);
+        switch (operator)
         {
-            operations.set(i,operations.get(i).trim());
-            System.out.println(operations.get(i));
+            case "/":
+                result/= rigthOp;
+                break;
+            case "*":
+                result*= rigthOp;
+                break;
+            case "+":
+                result+= rigthOp;
+                break;
+            case "-":
+                result+= rigthOp;
+                break;
         }
 
-        System.out.println(operations.size());
 
-        Double result = nums.pollLast();
-        for (int i = 0; i < operations.size(); i++)
+        if(result%1==0)
+            return String.format("%.0f",result);
+        return String.format("%.4f",result);
+    }
+
+    private String evalSubQuery(String statement, String regexp)
+    {
+        Pattern pattern = Pattern.compile(regexp);
+        Matcher matcher = pattern.matcher(statement);
+        while (matcher.find())
         {
-            Double operand = nums.pollLast();
-            switch (operations.get(i))
-            {
-                case "+":
-                    result+= operand;
-                    break;
-                case "-":
-                    result-= operand;
-                    break;
-                case "*":
-                    result*= operand;
-                    break;
-                case "/":
-                    result/= operand;
-                    break;
-                case "(":
-                    break;
-                case ")":
-                    break;
-                default:
-                    return null;
-            }
+            String subQuery = matcher.group(0);
+            System.out.println("OLD2="+statement);
+            statement = statement.replace(subQuery,evaluate(subQuery));
+            System.out.println("NEW2="+statement);
         }
-        System.out.println(result);
-        return "5";
+        return statement;
     }
 
 }
